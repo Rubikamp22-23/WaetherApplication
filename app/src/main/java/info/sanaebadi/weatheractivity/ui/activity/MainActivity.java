362ee411c2ec00e84f2430b7cbea3a75fbc840bb
sanaebadi <sana@journeyhero.net>
2023-03-06 19:56:08 +0200

    added naviagtion drawer items

--- app/src/main/java/info/sanaebadi/weatheractivity/ui/activity/MainActivity.java
+++ app/src/main/java/info/sanaebadi/weatheractivity/ui/activity/MainActivity.java
@@ -1,18 +1,27 @@
 package info.sanaebadi.weatheractivity.ui.activity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import java.util.Objects;
+
 import info.sanaebadi.weatheractivity.R;
+import info.sanaebadi.weatheractivity.base.BaseActivity;
 import info.sanaebadi.weatheractivity.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     private ActivityMainBinding binding;
+
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +32,21 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
 
         NavigationUI.setupWithNavController(binding.bottomNavigation, navController);
+
+        drawerLayout = binding.drawerLayout;
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

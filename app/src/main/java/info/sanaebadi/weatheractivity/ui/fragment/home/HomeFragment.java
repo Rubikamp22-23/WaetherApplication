235fedbd5b5919bc53747f80bab4ef614b450d6f
sanaebadi <sana@journeyhero.net>
2023-03-06 20:52:11 +0200

    added fragments tiday and forecast

---
+++ app/src/main/java/info/sanaebadi/weatheractivity/ui/fragment/home/HomeFragment.java
@@ -0,0 +1,30 @@
+package info.sanaebadi.weatheractivity.ui.fragment.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import info.sanaebadi.weatheractivity.base.BaseFragment;
+import info.sanaebadi.weatheractivity.databinding.FragmentHomeBinding;
+
+
+public class HomeFragment extends BaseFragment {
+
+    private FragmentHomeBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}

5487e4da855347dd74e885f99753aff3f70557aa
sanaebadi <sana@journeyhero.net>
2023-03-13 20:29:04 +0200

    added tablayout and viewpager in home fragment

--- app/src/main/java/info/sanaebadi/weatheractivity/ui/fragment/home/HomeFragment.java
+++ app/src/main/java/info/sanaebadi/weatheractivity/ui/fragment/home/HomeFragment.java
@@ -4,18 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.android.material.tabs.TabLayout;
+
 import info.sanaebadi.weatheractivity.base.BaseFragment;
 import info.sanaebadi.weatheractivity.databinding.FragmentHomeBinding;
+import info.sanaebadi.weatheractivity.ui.adapter.HomeViewPagerAdapter;
 
 
 public class HomeFragment extends BaseFragment {
 
     private FragmentHomeBinding binding;
 
+    private HomeViewPagerAdapter homeViewPagerAdapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -26,5 +31,29 @@ public class HomeFragment extends BaseFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        binding.tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        homeViewPagerAdapter = new HomeViewPagerAdapter(getChildFragmentManager(), 2, getContext());
+        binding.viewPager.setAdapter(homeViewPagerAdapter);
+
+        binding.viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(binding.tabLayout));
+
+        binding.tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Toast.makeText(getActivity(), tab.getText(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
